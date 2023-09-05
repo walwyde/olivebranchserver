cc8dc30a06dab562b03cdf404a91c71e74ec586f
walwyde <walwyde@gmail.com>
2023-09-05 14:42:43 +0100

    na wa o

--- utils/db.js
+++ utils/db.js
@@ -3,9 +3,11 @@ const dotenv = require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    console.log(process.env.MONGO_URI)
     const connetion = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+     
     });
     if (!connetion.connection) {
       process.abort();
